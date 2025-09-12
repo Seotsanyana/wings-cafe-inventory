c2338982f32dfadf3931b535c744db649911d665
Seotsanyana <monyauseotsanyana7@gmail.com>
2025-09-12 20:35:19 +0200

    Initial commit

--- wings-cafe-invetory-system/vite.config.js
+++ wings-cafe-invetory-system/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/WINGS-CAFE-INVENTORY"
+  base: process.env.NODE_ENV === 'production'
+    ? '/WINGS-CAFE-INVENTORY/'
+    : '/',
 })
 
