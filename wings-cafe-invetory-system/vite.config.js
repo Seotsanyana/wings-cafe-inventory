41a41061cb31d6e7d4ca08e23912e0f8d72cd63e
Your Name <your.email@example.com>
2025-09-15 17:51:49 +0200

    Initial commit: Wings Cafe Inventory System

--- wings-cafe-invetory-system/vite.config.js
+++ wings-cafe-invetory-system/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/WINGS-CAFE-INVENTORY/'
-    : '/',
-})
+  base: "/wings-cafe-inventory",
+});
 
